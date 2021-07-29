e1ed8da596ed5b47b51b0b9f53e040b524c7f8a1
David <davidtucu@hotmail.com>
2021-07-29 19:41:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+function App() {
+  return (
+    <div>
+     <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
